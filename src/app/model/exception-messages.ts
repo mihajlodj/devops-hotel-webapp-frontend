20a10be24c63d203f724f35249b267a75e1e53c6
Mihajlo Đorđević <mihajlodjordjevic931@gmail.com>
2024-06-01 17:00:18 +0200

    feat: Added notification checkbox to disable/enable notifications, to user profile

--- src/app/model/exception-messages.ts
+++ src/app/model/exception-messages.ts
@@ -6,5 +6,6 @@ export class ExceptionMessages {
   public static DELETE_PROFILE_ERROR: string = "Error when deleting user profile";
   public static PUT_PROFILE_ERROR: string = "Error when updating user profile";
   public static PASSWORD_CHANGE_ERROR: string = "Error when changing user password";
+  public static NOTIFICATION_TOGGLE_ERROR: string = "Error when changing user notifications";
 
 }
