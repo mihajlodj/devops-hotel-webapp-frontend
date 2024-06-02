872f5195fe47ab4ee71c248c32388962997143b9
Mihajlo Đorđević <mihajlodjordjevic931@gmail.com>
2024-06-02 12:02:59 +0200

    feat: added lodge apiurl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  userService: "http://localhost:8080",
-  apiUrl: 'http://localhost:8080/api'
+  userService: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/api',
+  userApiUrl: 'http://localhost:8080/api/users',
+  lodgeApiUrl: 'http://localhost:8081/api/lodge'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  userService: "",
-  apiUrl: ''
+  userService: '',
+  apiUrl: '',
+  userApiUrl: '',
+  lodgeApiUrl: ''
 };
