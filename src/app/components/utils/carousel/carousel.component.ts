6e0c7c505b9d27709f44e96940c1e7a5cdf03e21
Mihajlo Đorđević <mihajlodjordjevic931@gmail.com>
2024-06-08 13:23:17 +0200

    fix: simplified carousel logic

--- src/app/components/utils/carousel/carousel.component.ts
+++ src/app/components/utils/carousel/carousel.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { throwError } from 'rxjs';
 import { Photo } from 'src/app/model/photo/photo';
 import { environment } from 'src/environments/environment';
 
@@ -26,36 +25,13 @@ export class CarouselComponent {
 
   setShowPhotoCarouselBooleans() {
     if (this.photos.length !== 0 && this.urls.length !== 0) {
-      throw new Error("Having both: photos and image data not supported!");
-      return;
+      throw new Error("Having both: photos and image data is not supported!");
     }
     for (let i = 0; i < this.photos.length; i++) {
       this.show.push(i === 0);
     }
     for (let i = 0; i < this.urls.length; i++) {
       this.show.push(i === 0);
-    }
-  }
-
-  showNext() {
-    for (let i = 0; i < this.show.length; i++) {
-      if (this.show[i] === true) {
-        if (i < this.show.length - 1) { // not last photo
-          this.show[i+1] = true;
-          this.show[i] = false;
-        }
-      }
-    }
-  }
-
-  showPrevious() {
-    for (let i = 0; i < this.show.length; i++) {
-      if (this.show[i] === true) {
-        if (i > 0) { // not first photo
-          this.show[i] = false;
-          this.show[i-1] = true;
-        }
-      }
     }
   }
 }
