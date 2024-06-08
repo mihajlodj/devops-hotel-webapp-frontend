73e69a9503503bbf91a61859b1dc3b145b76ad75
Mihajlo Đorđević <mihajlodjordjevic931@gmail.com>
2024-06-08 13:23:01 +0200

    feat: Added lodge availability period crud

--- src/app/components/lodging/edit/lodging.edit.component.ts
+++ src/app/components/lodging/edit/lodging.edit.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { AsyncValidator } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
+import { LodgeAvailabilityPeriod } from 'src/app/model/lodge/availability-period';
 import { Lodge } from 'src/app/model/lodge/lodge';
+import { PriceType } from 'src/app/model/lodge/price-type';
 import { UserRole } from 'src/app/model/user/user-role';
 import { CurrentUserService } from 'src/app/services/current-user.service';
 import { LodgingService } from 'src/app/services/lodging.service';
@@ -17,6 +20,10 @@ export class LodgingEditComponent {
 
   photoUrlPrefix: string = environment.lodgePhotoUrl;
   lodge: Lodge = new Lodge();
+  availabilityPeriods: LodgeAvailabilityPeriod[] = [];
+
+  newPriceTypeLabel: string = 'Price type';
+  newAvailabilityPeriod: LodgeAvailabilityPeriod = new LodgeAvailabilityPeriod();
 
   isLoggedIn: boolean = false;
   isHost: boolean = false;
@@ -53,6 +60,15 @@ export class LodgingEditComponent {
           }
         }
       });
+      this.lodgingService.getAvalabilityPeriods(id).subscribe({
+        next: (response) => {
+          this.availabilityPeriods = response;
+        },
+        error: (err) => {
+          console.log(err);
+          alert(err.message);
+        }
+      });
     }
   }
   setIsOwner() {
@@ -65,5 +81,52 @@ export class LodgingEditComponent {
   checkRoles(){
     this.isGuest = this.currentUserService.checkUserRole(UserRole.GUEST);
     this.isHost = this.currentUserService.checkUserRole(UserRole.HOST)
+  }
+
+  setPriceType(label: string) {
+    this.newPriceTypeLabel = label;
+    if (label === 'Per guest') {
+      this.newAvailabilityPeriod.priceType = PriceType.PER_GUEST
+    } else {
+      this.newAvailabilityPeriod.priceType = PriceType.PER_LODGE
+    }
+  }
+
+  observerOrNext(alertMessage: string) {
+    return {
+      next: (_: any) => {
+        alert(alertMessage);
+        location.reload();
+      },
+      error: (err: any) => {
+        console.log(err);
+        alert(err.message);
+      }
+    }
+  }
+
+  changePriceType(aPeriod: LodgeAvailabilityPeriod, perWhat: string) {
+    if (perWhat === 'Per guest') {
+      aPeriod.priceType = PriceType.PER_GUEST;
+    } else {
+      aPeriod.priceType = PriceType.PER_LODGE;
+    }
+  }
+
+  addPeriod() {
+    this.newAvailabilityPeriod.lodgeId = this.lodge.id;
+    this.lodgingService.createAvailabilityPeriod(this.newAvailabilityPeriod).subscribe(
+      this.observerOrNext('Availability period added'));
+  }
+
+  deleteAPeriod(id: string) {
+    this.lodgingService.deleteAvailabilityPeriod(id).subscribe(
+      this.observerOrNext('Availability period deleted')
+    );
+  }
+  saveAPeriod(aPeriod: LodgeAvailabilityPeriod) {
+    this.lodgingService.updateAvailabilityPeriod(aPeriod.id, aPeriod).subscribe(
+      this.observerOrNext('Availability period updated')
+    )
   }
 }
--- src/app/services/lodging.service.ts
+++ src/app/services/lodging.service.ts
@@ -8,6 +8,7 @@ import { ExceptionMessages } from '../model/exception-messages';
 import { User } from '../model/user/user';
 import { environment } from 'src/environments/environment';
 import { Lodge } from '../model/lodge/lodge';
+import { LodgeAvailabilityPeriod } from '../model/lodge/availability-period';
 
 @Injectable({
   providedIn: 'root'
@@ -30,8 +31,7 @@ export class LodgingService {
   }
 
   getAll(): Observable<Lodge[]> {
-    return this.httpClient.get<Lodge[]>(this.apiUrl + '/all', {
-    }).pipe(catchError(err => {
+    return this.httpClient.get<Lodge[]>(this.apiUrl + '/all').pipe(catchError(err => {
       let message = 'Error: Getting lodges failed';
         if(err.error){
           message = err.error.message;
@@ -41,8 +41,7 @@ export class LodgingService {
   }
 
   get(id: string): Observable<Lodge> {
-    return this.httpClient.get<Lodge>(this.apiUrl + '/' + id, {
-    }).pipe(catchError(err => {
+    return this.httpClient.get<Lodge>(this.apiUrl + '/' + id).pipe(catchError(err => {
       let message = 'Error: Getting lodge failed';
         if(err.error){
           message = err.error.message;
@@ -52,8 +51,7 @@ export class LodgingService {
   }
 
   getMineAll(): Observable<Lodge[]> {
-    return this.httpClient.get<Lodge[]>(this.apiUrl + '/mine/all', {
-    }).pipe(catchError(err => {
+    return this.httpClient.get<Lodge[]>(this.apiUrl + '/mine/all').pipe(catchError(err => {
       let message = 'Error: Getting lodges failed';
         if(err.error){
           message = err.error.message;
@@ -62,9 +60,8 @@ export class LodgingService {
     }));
   }
 
-  delete(id: number): Observable<Lodge> {
-    return this.httpClient.delete<Lodge>(this.apiUrl + '/delete/' + id, {
-    }).pipe(catchError(err => {
+  delete(id: string): Observable<Lodge> {
+    return this.httpClient.delete<Lodge>(this.apiUrl + '/delete/' + id).pipe(catchError(err => {
       let message = 'Error: Deleting lodge failed';
         if(err.error){
           message = err.error.message;
@@ -73,4 +70,40 @@ export class LodgingService {
     }));
   }
 
+  getAvalabilityPeriods(id: string): Observable<LodgeAvailabilityPeriod[]> {
+    return this.httpClient.get<LodgeAvailabilityPeriod[]>(this.apiUrl + '/availability/all/' + id).pipe(catchError(err => {
+      let message = "Error: Getting lodge availability periods failed";
+      if (err.error) {
+        message = err.error.message;
+      }
+      return throwError(() => new Error(message));
+    }));
+  }
+  createAvailabilityPeriod(aPeriod: LodgeAvailabilityPeriod): Observable<LodgeAvailabilityPeriod> {
+    return this.httpClient.post<LodgeAvailabilityPeriod>(this.apiUrl + '/availability/create/', aPeriod).pipe(catchError(err => {
+      let message = "Error: Creating lodge availability period failed";
+      if (err.error) {
+        message = err.error.message;
+      }
+      return throwError(() => new Error(message));
+    }));
+  }
+  deleteAvailabilityPeriod(id: string): Observable<LodgeAvailabilityPeriod> {
+    return this.httpClient.delete<LodgeAvailabilityPeriod>(this.apiUrl + '/availability/' + id).pipe(catchError(err => {
+      let message = "Error: Deleting lodge availability period failed";
+      if (err.error) {
+        message = err.error.message;
+      }
+      return throwError(() => new Error(message));
+    }));
+  }
+  updateAvailabilityPeriod(id: string, aPeriod: LodgeAvailabilityPeriod): Observable<LodgeAvailabilityPeriod> {
+    return this.httpClient.put<LodgeAvailabilityPeriod>(this.apiUrl + '/availability/' + id, aPeriod).pipe(catchError(err => {
+      let message = "Error: Updating lodge availability period failed";
+      if (err.error) {
+        message = err.error.message;
+      }
+      return throwError(() => new Error(message));
+    }));
+  }
 }
