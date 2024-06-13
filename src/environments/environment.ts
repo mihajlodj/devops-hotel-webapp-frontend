8ed678ad58b1e05677e8f0a0efac216c57263ecf
Vuk Vukovic <vuk.vukovic.2000@gmail.com>
2024-06-13 20:55:15 +0200

    feat: Dockerfile and push to Docker Hub (#10)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  userService: '',
-  apiUrl: '',
-  userApiUrl: '',
-  lodgePhotoUrl: '',
-  lodgeApiUrl: '',
-  reservationApiUrl: '',
-  ratingApiUrl: ''
+  userService: 'http://localhost:8080',
+  apiUrl: 'http://localhost:8080/api',
+  lodgePhotoUrl: 'http://localhost:8081',
+  userApiUrl: 'http://localhost:8080/api/users',
+  lodgeApiUrl: 'http://localhost:8081/api/lodge',
+  reservationApiUrl: 'http://localhost:8082/api/reservation',
+  ratingApiUrl: 'http://localhost:8083/api/ratings'
 };
